5f0ccf8fe325b6468dd140f9901c1ec01e3cdec9
jgarnier <julien.garnier@lirmm.fr>
2026-01-17 09:22:34 +0100

    move files

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// Configuration globale pour l'auto-inscription IANSEO
+// Ce fichier ne doit pas être accessible directement depuis le web
+
+// Empêcher l'accès direct
+if (!defined('CONFIG_ACCESS')) {
+    die('Accès direct interdit');
+}
+?>
